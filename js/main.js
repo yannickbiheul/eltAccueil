024bc0c76383c52f26a38a6a1b9c7ab1fbb06ca5
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-30 18:19:06 +0200

    JavaScript du menu Burger

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* MENU BURGER */
+
+let boutonBurger = document.querySelector('.boutonBurger');
+let menuBurger = document.querySelector('.menuBurger');
+let elts = document.querySelectorAll('.elt');
+
+boutonBurger.addEventListener('click', function() {
+    menuBurger.classList.toggle("menuBurgerOpen");
+})
+
+for (let i = 0; i < elts.length; i++) {
+    elts[i].addEventListener('click', function() {
+        menuBurger.classList.toggle("menuBurgerOpen");
+    })
+}
