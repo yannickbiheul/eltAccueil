b099b72b161a23ba22dfcca9825d1bf47547d0a9
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-10 09:09:38 +0200

    Ajout d'un code pour trouver la hauteur de l'écran

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 AOS.init();
 
-/* MENU BURGER */
-
-let boutonBurger = document.querySelector('.boutonBurger');
-let menuBurger = document.querySelector('.menuBurger');
-let elts = document.querySelectorAll('.elt');
-
-boutonBurger.addEventListener('click', function() {
-    menuBurger.classList.toggle("menuBurgerOpen");
-})
-
-for (let i = 0; i < elts.length; i++) {
-    elts[i].addEventListener('click', function() {
-        menuBurger.classList.toggle("menuBurgerOpen");
-    })
-}
+let accueil = document.querySelector("#accueil");
+console.log(window.getComputedStyle(accueil).height);
